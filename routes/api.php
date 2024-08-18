b312be18d1933954d6411858fb9c194e64490ed9
tks <87138608+takashiraki@users.noreply.github.com>
2024-08-18 14:02:48 +0900

    feat: sign up. (#3)

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Http\Controllers\Auth\SignUpController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/signup', [SignUpController::class, 'handle']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
