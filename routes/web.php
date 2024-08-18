f9fe72290e9187edcbee8ba38425a5d837fd2aa1
takashirakki <stgtbike4649@gmail.com>
2024-08-18 10:48:10 +0000

    feat: sign out.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\Auth\SignInController;
+use App\Http\Controllers\Auth\SignOutController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [SignInController::class, 'handle']);
+Route::post('/logout', [SignOutController::class, 'handle']);
