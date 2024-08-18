36b992852550decd1cf23a72f04f7a5ab735b233
tks <87138608+takashiraki@users.noreply.github.com>
2024-08-18 14:22:00 +0900

    Feature/signin (#4)
    * add: sign in route.
    
    * feat: sign in.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Http\Controllers\Auth\SignInController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('/login', [SignInController::class, 'handle']);
